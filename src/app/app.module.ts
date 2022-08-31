56d8874843e7bb683b145d12f39d0fba4194ecb3
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-08-31 14:02:52 +0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './homepage/shared/components/header/header.component';
+import { HeroComponent } from './homepage/shared/components/hero/hero.component';
+import { SymptomesComponent } from './homepage/shared/components/symptomes/symptomes.component';
+import { PickupComponent } from './homepage/shared/components/pickup/pickup.component';
+import { AboutComponent } from './homepage/shared/components/about/about.component';
+import { IngridientsComponent } from './homepage/shared/components/ingridients/ingridients.component';
+import { WorkwayComponent } from './homepage/shared/components/workway/workway.component';
+import { ClientsComponent } from './homepage/shared/components/clients/clients.component';
+import { QuestionsComponent } from './homepage/shared/components/questions/questions.component';
+import { MapComponent } from './homepage/shared/components/map/map.component';
+import { FooterComponent } from './homepage/shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    SymptomesComponent,
+    PickupComponent,
+    AboutComponent,
+    IngridientsComponent,
+    WorkwayComponent,
+    ClientsComponent,
+    QuestionsComponent,
+    MapComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/homepage/shared/components/workway/workway.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workway',
+  templateUrl: './workway.component.html',
+  styleUrls: ['./workway.component.scss']
+})
+export class WorkwayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
