bb4ded30bcbf93dc3f1def306e7a6715ebdcca7e
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-09-01 13:17:29 +0300

    symptomes markup and some logic

--- src/app/homepage/shared/components/symptomes/symptomes.component.ts
+++ src/app/homepage/shared/components/symptomes/symptomes.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Symptomes {
+  src: string;
+  id?: number;
+  description: string;
+}
+
 @Component({
   selector: 'app-symptomes',
   templateUrl: './symptomes.component.html',
-  styleUrls: ['./symptomes.component.scss']
+  styleUrls: ['./symptomes.component.scss'],
 })
 export class SymptomesComponent implements OnInit {
+  symptomes: Symptomes[] = [
+    {
+      src: '',
+      id: 1,
+      description: 'Сильное натуживание',
+    },
+    {
+      src: '',
+      id: 2,
+      description: 'Редкая и трудная дефекация',
+    },
+    {
+      src: '',
+      id: 3,
+      description: 'Боль в животе',
+    },
+    {
+      src: '',
+      id: 4,
+      description: 'Урчание',
+    },
+    {
+      src: '',
+      id: 5,
+      description: 'Ощущение неполного опорожнения',
+    },
+    {
+      src:'',
+      id: 6,
+      description:'Тяжесть в животе'
+      },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
